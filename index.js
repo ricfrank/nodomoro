5bec54c64b13da1641f807abb4ce57a37a0945c3
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2012-03-17 12:29:46 +0100

    Updated paths in index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var server = require("./server");
-var requestHandlers = require("./requestHandlers");
+var path = require('path');
+
+var APP_PATH = {
+	SERVER: "./server",
+	MODEL: "./model"
+}
+
+var server = require(path.join(APP_PATH.SERVER, "server"));
+var requestHandlers = require(path.join(APP_PATH.MODEL,"requestHandlers"));
 
 var handle = {}
 
 //server.start(router.route, handle);
 server.start(handle);
-    
