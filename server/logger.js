3a03bb18157ae045eb8b5fa905792eb07dacc4cb
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2012-03-17 14:02:07 +0100

    Added pub/sub style feature for logger

--- server/logger.js
+++ server/logger.js
@@ -1,5 +1,17 @@
-function log(message) {
-    console.log(message);
+var token = 0;
+var subscribers = [];
+
+function subscribe(name){
+	var subscriber = {
+		name: name
+	}
+	subscribers[token] = subscriber;
+	return token
+}
+
+function log(message, token) {
+    console.log("[" + subscribers[token].name + "] " + message);
 }
 
 exports.log = log;
+exports.subscribe = subscribe;
--- server/server.js
+++ server/server.js
@@ -9,14 +9,16 @@ var url = require("url");
 *  - port: listen port, (default 8888)
 */
 function start(params) {
+    var SERVER_LOG_TOKEN = params.logger.subscribe("SERVER");
+
     function onRequest(request, response){
         var pathname = url.parse(request.url).pathname;
-        params.logger.log("Request received: " + pathname);
+        params.logger.log("Request received: " + pathname, SERVER_LOG_TOKEN);
         params.route(params.handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
-    params.logger.log("Server started");
+    params.logger.log("Server started", SERVER_LOG_TOKEN);
 }
 
 exports.start = start;
