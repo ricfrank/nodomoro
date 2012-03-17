f1992d950b3cb901a9702d7116892ccaa20a90d4
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2012-03-17 12:52:43 +0100

    Renamed handlers file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var APP_PATH = {
 
 var server   = require(APP_PATH.SERVER + "server");
 var router   = require(APP_PATH.SERVER + "router");
-var handlers = require(APP_PATH.SERVER + "requestHandlers");
+var handlers = require(APP_PATH.SERVER + "handlers");
 
 var handle = {
 	"/": handlers.init
